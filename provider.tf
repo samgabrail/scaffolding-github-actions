45d24225325e44ec1ee360ac4b98db8c85367339
Aladdin Almubayed <aladdin.almubayed@resourcely.io>
2024-02-23 10:19:15 -0800

    add provider example to the scaffolding

---
+++ provider.tf
@@ -0,0 +1,21 @@
+// Configure the provider(s) for your cloud platform(s).
+//
+// See https://registry.terraform.io/browse/providers for a full list
+// of supported providers and configuration instructions.
+//
+// provider "<foo>" {
+//    ...
+// }
+
+// # Example:
+// provider "aws" {
+//   region = "us-east-1"
+// 
+//   skip_credentials_validation = true
+//   skip_metadata_api_check     = true
+//   skip_requesting_account_id  = true
+// }
+
+// provider "github" {}
+
+// provider "google" {}
